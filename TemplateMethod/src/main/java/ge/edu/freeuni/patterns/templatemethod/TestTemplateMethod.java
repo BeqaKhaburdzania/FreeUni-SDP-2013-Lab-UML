7aa3fe3884f597349a00e5d9cc8a0879a9bc0787
Ioseb Dzmanashvili <ioseb.dzmanashvili@azry.com>
2014-03-21 03:59:50 +0400

    [#21] implemented strategy pattern...

---
+++ TemplateMethod/src/main/java/ge/edu/freeuni/patterns/templatemethod/TestTemplateMethod.java
@@ -0,0 +1,11 @@
+package ge.edu.freeuni.patterns.templatemethod;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ioseb
+ * Date: 3/21/14
+ * Time: 3:26 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestTemplateMethod {
+}
