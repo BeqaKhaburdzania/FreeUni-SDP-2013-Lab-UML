d54163ceb123dc47675be2c29c0e932f6e1ca19d
MT13 <maria.tarielashvili@gmail.com>
2015-03-17 22:15:22 +0400

    abstract factory part added

---
+++ ViewHandler/src/ChatView.java
@@ -0,0 +1,26 @@
+
+public class ChatView extends AbstractView {
+	
+	public ChatView(){
+		
+	}
+
+	@Override
+	public String init() {
+		return name+": I am booorn!";
+		
+	}
+
+	@Override
+	public String resize() {
+		return name+": I was resized!";
+		
+	}
+
+	@Override
+	public String update() {
+		return name+ ": Somebody updated me!";
+		
+	}
+
+}
---
+++ ViewHandler/src/VideoView.java
@@ -0,0 +1,26 @@
+
+public class VideoView extends AbstractView{
+	
+	public VideoView(){
+		
+	}
+
+	@Override
+	public String init() {
+		return name+": oh, new world!";
+		
+	}
+
+	@Override
+	public String resize() {
+		return name+": I definitely must start a new diet!";
+		
+	}
+
+	@Override
+	public String update() {
+		return name+": I smell changes...";
+		
+	}
+
+}
