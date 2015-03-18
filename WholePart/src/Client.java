8133f0799fca6682f24d416f80d6d23c61d8c5a7
Ketevan Uridia <Ketevan Uridia@KetevanUridia>
2015-03-18 22:15:50 +0400

    #55

--- WholePart/src/Client.java
+++ WholePart/src/Client.java
@@ -3,12 +3,11 @@ public class Client {
 
 	
 	public void test() {
-		MenuComponent pancakeHouseMenu = new Menu("PANCAKE HOUSE MENU",
-				"Breakfast");
+		
+		MenuComponent pancakeHouseMenu = new Menu("PANCAKE HOUSE MENU","Breakfast");
 		MenuComponent dinerMenu = new Menu("DINER MENU", "Lunch");
 		MenuComponent cafeMenu = new Menu("CAFE MENU", "Dinner");
-		MenuComponent dessertMenu = new Menu("DESSERT MENU",
-				"Dessert of course!");
+		MenuComponent dessertMenu = new Menu("DESSERT MENU","Dessert of course!");
 		MenuComponent allMenus = new Menu("ALL MENUS", "All menus combined");
 		allMenus.add(pancakeHouseMenu);
 		allMenus.add(dinerMenu);
